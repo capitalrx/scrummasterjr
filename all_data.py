307240412982b993a37a526bca090fe2bfcedf85
Marisa Gryczan <marisagryczan@cap-rx.com>
2025-12-09 14:08:25 -0700

    Update all_data.py
    Co-authored-by: Chris Gamio <54854054+cgamio@users.noreply.github.com>

--- all_data.py
+++ all_data.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         "Vigilante": 784,
         "Platform": 388,
         "Rosetta": 586,
-        "T3quila": 1347
+        "T3quila": 1347,
         "Care Bears": 1675
     }
 
